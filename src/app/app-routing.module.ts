0cc9b3eb755861efeffb9033c53584f115d8558a
권진오 <kjo133@naver.com>
2018-11-30 14:55:26 +0900

    메인화면 라우트 연결

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { LoginComponent } from './common/login.component';
+import { MainComponent } from './common/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path : '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent,
+  //   data: {title: '로그인'}
+  // },
+  {
+    path : 'main',
+    component : MainComponent,
+    data: {title: '메인'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponent = [
+  //LoginComponent, 
+  MainComponent]
